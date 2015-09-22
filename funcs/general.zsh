a984afe6c6e2da5e8b5088ca821a0d1323657e3b
Russell Myers <rmmyers@amazon.com>
2015-09-22 14:57:03 -0400

    Add general functions.

---
+++ funcs/general.zsh
@@ -0,0 +1,40 @@
+echo "Loading general functions..."
+
+# opens a browser window
+function browse {
+    if [[ $platform == 'cygwin' ]]; then
+        /cygdrive/c/Windows/explorer.exe /e,`cygpath -w "$1"`
+    elif [[ $platform == 'linux' ]]; then
+        nautilus "$1"
+    fi
+}
+
+function v () {
+    gvim --remote "$1" .
+}
+
+function auth () {
+    ssh-add ~/.ssh/id_rsa
+}
+
+function untar ()
+{
+    ar xvfj "$1"
+}
+
+function gcr ()
+{
+    echo "grep -rin ${@:1} ."
+    grep -rin ${@:1} .
+}
+
+function ifind()
+{
+    # echo "find . -iname ${@:1}"
+    find . -iname ${@:1}
+}
+
+function drop-caches()
+{
+    sudo bash -c "echo 3 > /proc/sys/vm/drop_caches"
+}
